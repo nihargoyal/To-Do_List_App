14f0a79dd31ecbfe454c9a9520845c5d934136b6
Nihar Goyal <goyalnihar786@gmail.com>
2023-06-28 03:01:13 +0530

    Update serializers.py

--- todolist/tapp/serializers.py
+++ todolist/tapp/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Todo
+from .models import Todo, Tag
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
 
 class TaskSerializer(serializers.ModelSerializer):
+    tag = TagSerializer(many=True)
+
     class Meta:
         model = Todo
         fields = '__all__'
